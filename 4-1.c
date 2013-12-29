4e4fa75842adf6ca88e86e60cc4a94b200b4b0d5
Afaq Mammadli <afaqmammadli@yahoo.com>
2013-12-29 21:11:02 +0400

    4-1 in formattingi duzeltdim

--- 4-1.c
+++ 4-1.c
@@ -3,24 +3,24 @@
 /* strindex: return index of t in s, -1 if none*/
 int strindex(char s[], char t[])
 {
-    int i, j, k;
+  int i, j, k;
 
-    for (i = 0; s[i] != '\0'; i++){
-        for(j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
-            ;
-        if (k > 0 && t[k] == '\0')
-            return i;
-     }
-     return -1;
+  for (i = 0; s[i] != '\0'; i++){
+    for(j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+      ;
+    if (k > 0 && t[k] == '\0')
+      return i;
+  }
+  return -1;
 }
 
-main()
-{char pattern[] = "ow";
-char sent[] = "how could you own it?";
+main() {
+  char pattern[] = "ow";
+  char sent[] = "how could you own it?";
 
   int i = strindex(sent, pattern)
-            printf("%int",i );
+      printf("%int",i );
            
        
-    return 0;
+  return 0;
 }
